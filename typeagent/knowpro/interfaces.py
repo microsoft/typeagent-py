c578db335cdc1339b8a175f270b0c80d1ea4efeb
gvanrossum-ms <gvanrossum@microsoft.com>
2025-03-10 17:25:23 -0700

    [python/knowpro] Add build infrastructure; changed directory structure (#808)
    The old package structure (kp/knowpro/interfaces.py) was missing a
    directory level where to put the build infra files, notably
    pyproject.toml. I renamed things as follows: kp/knowpro->
    ta/typeagent/knowpro (and similar for kp/memconv). Now, to build, you
    `cd ta` and then `make all`. It takes care of venv creation and build
    dependencies. The platform-independent wheel file ends up under dist/.
    
    The build infra itself consists of a pyproject.toml file which
    configures the build, and a Makefile containing simple commands to run
    the build. `make help` explains the options. There are also a separate
    LICENSE file (copy from root LICENSE) and a README.md file. There are no
    longer separate READMEs in the sub-packages knowpro and memconv.
    
    I have only tested this on Ubuntu 22 under WSL2. For Windows we might
    need something other than Makefile (since I believe Make isn't well
    supported on Windows). But you can just run the commands from the
    Makefile directly.
    
    A hidden development dependency is pyright -- you can install it using
    `npm install -g pyright` .

---
+++ typeagent/knowpro/interfaces.py
@@ -0,0 +1,372 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# TODO:
+# - See TODOs in kplib.py.
+# - Do the Protocol classes need to be @runtime_checkable?
+#
+# NOTE:
+# - I took some liberty with index types and made them int.
+# - I rearranged the order in some cases to ensure def-before-ref.
+# - I translated readonly to @property.
+
+from collections.abc import Sequence
+from dataclasses import dataclass, field
+from datetime import datetime as Datetime
+from typing import Any, Callable, Literal, Protocol, runtime_checkable
+
+from . import kplib
+
+
+# An object that can provide a KnowledgeResponse structure.
+@runtime_checkable
+class IKnowledgeSource(Protocol):
+    def get_knowledge(self) -> kplib.KnowledgeResponse:
+        raise NotImplementedError
+
+
+@dataclass
+class DeletionInfo:
+    timestamp: str
+    reason: str | None = None
+
+
+type MessageIndex = int
+
+
+@runtime_checkable
+class IMessage(IKnowledgeSource, Protocol):
+    # The text of the message, split into chunks.
+    text_chunks: list[str]
+    timestamp: str | None = None
+    tags: list[str]
+    deletion_info: DeletionInfo | None = None
+
+
+type SemanticRefIndex = int
+
+
+@dataclass
+class ScoredSemanticRef:
+    semantic_ref_index: SemanticRefIndex
+    score: float
+
+
+@dataclass
+class ScoredMessageIndex:
+    message_index: MessageIndex
+    score: float
+
+
+@runtime_checkable
+class ITermToSemanticRefIndex(Protocol):
+    def get_terms(self) -> Sequence[str]:
+        raise NotImplementedError
+
+    def add_term(
+        self,
+        term: str,
+        semantic_ref_index: SemanticRefIndex | ScoredSemanticRef,
+    ) -> None:
+        raise NotImplementedError
+
+    def remove_term(self, term: str, semantic_ref_index: SemanticRefIndex) -> None:
+        raise NotImplementedError
+
+    def lookup_term(self, term: str) -> Sequence[ScoredSemanticRef] | None:
+        raise NotImplementedError
+
+
+type KnowledgeType = Literal["entity", "action", "topic", "tag"]
+
+
+@dataclass
+class Topic:
+    text: str
+
+
+@dataclass
+class Tag:
+    text: str
+
+
+type Knowledge = kplib.ConcreteEntity | kplib.Action | Topic | Tag
+
+
+@dataclass
+class TextLocation:
+    # The index of the message.
+    message_index: MessageIndex
+    # The index of the chunk.
+    chunk_index: int = 0
+    # The index of the character within the chunk.
+    char_index: int = 0
+
+
+# A text range within a session.
+@dataclass
+class TextRange:
+    # The start of the range.
+    start: TextLocation
+    # The end of the range (exclusive).
+    end: TextLocation | None = None
+
+
+@dataclass
+class SemanticRef:
+    semantic_ref_index: SemanticRefIndex
+    range: TextRange
+    knowledge_type: KnowledgeType
+    knowledge: Knowledge
+
+
+@dataclass
+class DateRange:
+    start: Datetime
+    # Inclusive.  # TODO: Really? Shouldn't this be exclusive?
+    end: Datetime | None = None
+
+
+@dataclass
+class Term:
+    text: str
+    # Optional weighting for these matches.
+    weight: float | None = None
+
+
+@dataclass
+class ScoredKnowledge:
+    knowledge_type: KnowledgeType
+    knowledge: Knowledge
+    score: float
+
+
+# Allows for faster retrieval of name, value properties
+@runtime_checkable
+class IPropertyToSemanticRefIndex(Protocol):
+    def get_values(self) -> Sequence[str]:
+        raise NotImplementedError
+
+    def add_property(
+        self,
+        property_name: str,
+        value: str,
+        semantic_ref_index: SemanticRefIndex | ScoredSemanticRef,
+    ) -> None:
+        raise NotImplementedError
+
+    def lookup_property(
+        self, property_name: str, value: str
+    ) -> Sequence[ScoredSemanticRef] | None:
+        raise NotImplementedError
+
+
+@dataclass
+class TimestampedTextRange:
+    timestamp: str
+    range: TextRange
+
+
+# Return text ranges in the given date range.
+@runtime_checkable
+class ITimestampToTextRangeIndex(Protocol):
+    def add_timestamp(self, message_index: MessageIndex, timestamp: str) -> bool:
+        raise NotImplementedError
+
+    def add_timestamps(
+        self, message_imestamps: Sequence[tuple[MessageIndex, str]]
+    ) -> None:
+        raise NotImplementedError
+
+    def lookup_range(self, date_range: DateRange) -> Sequence[TimestampedTextRange]:
+        raise NotImplementedError
+
+
+@runtime_checkable
+class ITermToRelatedTerms(Protocol):
+    def lookup_term(self, text: str) -> Sequence[Term] | None:
+        raise NotImplementedError
+
+
+@runtime_checkable
+class ITermToRelatedTermsFuzzy(Protocol):
+    async def add_terms(
+        self, terms: Sequence[str], event_handler: "IndexingEventHandlers | None" = None
+    ) -> "ListIndexingResult":
+        raise NotImplementedError
+
+    async def lookup_term(
+        self,
+        text: str,
+        max_matches: int | None = None,
+        threshold_score: float | None = None,
+    ) -> Sequence[Term]:
+        raise NotImplementedError
+
+    async def lookup_terms(
+        self,
+        text_array: Sequence[str],
+        max_matches: int | None = None,
+        threshold_score: float | None = None,
+    ) -> Sequence[Sequence[Term]]:
+        raise NotImplementedError
+
+
+@runtime_checkable
+class ITermToRelatedTermsIndex(Protocol):
+    @property
+    def aliases(self) -> ITermToRelatedTerms | None:
+        raise NotImplementedError
+
+    @property
+    def fuzzy_index(self) -> ITermToRelatedTermsFuzzy | None:
+        raise NotImplementedError
+
+
+# A Thread is a set of text ranges in a conversation.
+@dataclass
+class Thread:
+    description: str
+    ranges: Sequence[TextRange]
+
+
+type ThreadIndex = int
+
+
+@dataclass
+class ScoredThreadIndex:
+    thread_index: ThreadIndex
+    score: float
+
+
+@runtime_checkable
+class IConversationThreads(Protocol):
+    @property
+    def threads(self) -> Sequence[Thread]:
+        raise NotImplementedError
+
+    async def add_thread(self, thread: Thread) -> None:
+        raise NotImplementedError
+
+    async def lookup_thread(
+        self,
+        thread_description: str,
+        max_matches: int | None = None,
+        threshold_score: float | None = None,
+    ) -> Sequence[ScoredThreadIndex] | None:
+        raise NotImplementedError
+
+
+@runtime_checkable
+class IConversationSecondaryIndexes(Protocol):
+    property_to_semantic_ref_index: IPropertyToSemanticRefIndex | None
+    timestamp_index: ITimestampToTextRangeIndex | None
+    term_to_related_terms_index: ITermToRelatedTermsIndex | None
+    threads: IConversationThreads | None
+    message_index: "IMessageTextIndex | None" = None
+
+
+@runtime_checkable
+class IConversation[TMessage: IMessage](Protocol):
+    name_tag: str
+    tags: list[str]
+    messages: list[TMessage]
+    semantic_refs: list[SemanticRef] | None
+    semantic_ref_index: ITermToSemanticRefIndex | None
+    secondary_indexes: IConversationSecondaryIndexes | None
+
+
+@runtime_checkable
+class IMessageTextIndex(Protocol):
+
+    async def add_messages(
+        self,
+        messages: list[IMessage],
+        event_handler: "IndexingEventHandlers | None" = None,
+    ) -> "ListIndexingResult":
+        raise NotImplementedError
+
+    async def lookup_messages(
+        self,
+        message_text: str,
+        max_matches: int | None = None,
+        threshold_score: float | None = None,
+    ) -> list[ScoredMessageIndex]:
+        raise NotImplementedError
+
+    async def lookup_messages_in_subset(
+        self,
+        message_text: str,
+        indices_to_search: list[MessageIndex],
+        max_matches: int | None = None,
+        threshold_score: float | None = None,
+    ) -> list[ScoredMessageIndex]:
+        raise NotImplementedError
+
+
+# ------------------------
+# Serialization formats
+# ------------------------
+
+
+@runtime_checkable
+class ITermToSemanticRefIndexItem(Protocol):
+    term: str
+    semantic_ref_indices: Sequence[ScoredSemanticRef]
+
+
+# Persistent form of a term index.
+@runtime_checkable
+class ITermToSemanticRefIndexData(Protocol):
+    items: Sequence[ITermToSemanticRefIndexItem]
+
+
+@runtime_checkable
+class IConversationData[TMessage](Protocol):
+    name_tag: str
+    messages: Sequence[TMessage]
+    tags: Sequence[str]
+    semantic_refs: Sequence[SemanticRef]
+    semantic_index_data: ITermToSemanticRefIndexData | None = None
+
+
+# ------------------------
+# Indexing
+# ------------------------
+
+
+@dataclass
+class IndexingEventHandlers:
+    on_knowledge_extracted: (
+        Callable[
+            [
+                TextLocation,  # chunk
+                kplib.KnowledgeResponse,  # knowledge_result
+            ],
+            bool,
+        ]
+        | None
+    ) = None
+    on_embeddings_created: (
+        Callable[
+            [
+                Sequence[str],  # source_texts
+                Sequence[str],  # batch
+                int,  # batch_start_at
+            ],
+            bool,
+        ]
+        | None
+    ) = None
+
+
+@dataclass
+class IndexingResults:
+    chunks_indexed_upto: TextLocation | None = None
+    error: str | None = None
+
+
+@dataclass
+class ListIndexingResult:
+    number_completed: int
+    error: str | None = None
---
+++ typeagent/knowpro/kplib.py
@@ -0,0 +1,82 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# TODO:
+# - What does the comment "Very concise values" in class Facet mean?
+# - Should the field names be camelCase to match the JSON schema?
+# - For things of type float, should we add `| int` to emphasize that int is okay?
+#   (Some users think float means float only.)
+
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class Quantity:
+    amount: float
+    units: str
+
+
+type Value = str | float | bool | Quantity
+
+
+@dataclass
+class Facet:
+    name: str
+    # Very concise values.
+    value: Value
+
+
+# Specific, tangible people, places, institutions or things only
+@dataclass
+class ConcreteEntity:
+    # The name of the entity or thing such as "Bach", "Great Gatsby",
+    # "frog" or "piano".
+    name: str
+    # The types of the entity such as "speaker", "person", "artist", "animal",
+    # "object", "instrument", "school", "room", "museum", "food" etc.
+    # An entity can have multiple types; entity types should be single words.
+    type: list[str]
+    # A specific, inherent, defining, or non-immediate facet of the entity
+    # such as "blue", "old", "famous", "sister", "aunt_of", "weight: 4 kg".
+    # Trivial actions or state changes are not facets.
+    # Facets are concise "properties".
+    facets: list[Facet] | None = None
+
+
+@dataclass
+class ActionParam:
+    name: str
+    value: Value
+
+
+type VerbTense = Literal["past", "present", "future"]
+
+
+@dataclass
+class Action:
+    # Each verb is typically a word.
+    verbs: list[str]
+    verb_tense: VerbTense
+    subject_entity_name: str | Literal["none"]
+    object_entity_name: str | Literal["none"]
+    indirect_object_entity_name: str | Literal["none"]
+    params: list[str | ActionParam] | None = None
+    # If the action implies this additional facet or property of the
+    # subject entity, such as hobbies, activities, interests, personality.
+    subject_entity_facet: Facet | None = None
+
+
+# Detailed and comprehensive knowledge response.
+@dataclass
+class KnowledgeResponse:
+    entities: list[ConcreteEntity]
+    # The 'subject_entity_name' and 'object_entity_name' must correspond
+    # to the 'name' of an entity listed in the 'entities' array.
+    actions: list[Action]
+    # Some actions can ALSO be expressed in a reverse way...
+    # E.g. (A give to B) --> (B receive from A) and vice versa.
+    # If so, also return the reverse form of the action, full filled out.
+    inverse_actions: list[Action]
+    # Detailed, descriptive topics and keywords.
+    topics: list[str]
