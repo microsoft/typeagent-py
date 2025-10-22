6d9e498fbb0ff8a11dbbcec27f655e68133e8d9e
wirthual <wirthra@gmail.com>
2025-10-21 18:52:25 -0700

    Add endpoint_envvar argument to AsyncEmbeddingModel (#36)
    Allow passing of `endpoint_envvar` into constructor.
    
    It must either match the envvar of the entry identified by the model_name argument in the table of predefined models, or the model_name argument must not be a key in the table.
    
    This allows passing parameters for an entirely new model (e.g. a local model with different parameters).

--- test/test_embeddings.py
+++ test/test_embeddings.py
@@ -128,3 +128,28 @@ async def test_refresh_auth(
 
     embedding_model.azure_token_provider.refresh_token.assert_called_once()  # type: ignore
     assert embedding_model.async_client is not None
+
+
+@pytest.mark.asyncio
+async def test_set_endpoint(monkeypatch):
+    """Test creating of model with custom endpoint."""
+
+    monkeypatch.setenv("OPENAI_API_KEY", "does-not-matter")
+    monkeypatch.setenv("INFINITY_EMBEDDING_URL", "http://localhost:7997")
+
+    embedding_model = AsyncEmbeddingModel(
+        1024, "custom_model", "INFINITY_EMBEDDING_URL"
+    )
+
+    assert embedding_model.async_client is not None
+    assert embedding_model.async_client.base_url == "http://localhost:7997"
+    assert embedding_model.async_client.api_key == "does-not-matter"
+
+    with pytest.raises(
+        ValueError,
+        match="Environment variable for embedding endpoint WRONG_ENDPOINT does not match required environment"
+        " variable AZURE_OPENAI_ENDPOINT_EMBEDDING_3_SMALL for embedding model text-embedding-small.",
+    ):
+        embedding_model = AsyncEmbeddingModel(
+            2000, "text-embedding-small", "WRONG_ENDPOINT"
+        )
